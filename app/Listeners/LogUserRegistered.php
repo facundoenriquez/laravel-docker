7498ca21e1959aeb51b65c9487e09f500c5ab8d6
Facundo Enriquez <fenriquez@inym.org.ar>
2025-09-23 12:03:17 -0300

    adding event to test

---
+++ app/Listeners/LogUserRegistered.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserRegistered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class LogUserRegistered
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(UserRegistered $event): void
+    {
+        ## registro en el logs
+        Log::info('Nuevo usuario registrado: ' . $event->msg);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Events\UserRegistered;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/prueba', function () {
+
+    UserRegistered::dispatch('Evento disparado');
+
+    return 'Hola Mundo con Laravel';
 });
