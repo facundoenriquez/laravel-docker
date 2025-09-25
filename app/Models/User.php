1198ff807518e812b22289927299f5a052346b56
Facundo Enriquez <fenriquez@inym.org.ar>
2025-09-25 13:51:12 -0300

    querys scopes

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function scopeName($query, $name)
+    {
+        if ($name) {
+            return $query->where('name', 'like', "%$name%");
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 
 Route::get('/prueba', function () {
 
-    $user = User::find(1);
-    Gate::forUser($user)->authorize('isAdmin');
+    $user = User::query();
+    $user->name('enriquez')->get();
     return $user;
 });
