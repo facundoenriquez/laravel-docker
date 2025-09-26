2e145dd6da0849ff3642bd8bfa642ada7bfe4c57
facundo enriquez <facundo.tomas.enriquez@gmail.com>
2025-09-25 21:33:59 -0300

    scopes

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 
 Route::get('/prueba', function () {
 
-    $user = User::query();
-    $user->name('enriquez')->get();
-    return $user;
+    $q = User::query();
+    $users =  $q->name('facundo')->get();
+    return $users;
 });
